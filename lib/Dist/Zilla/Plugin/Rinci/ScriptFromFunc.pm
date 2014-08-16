c0165f5fff84a85308cabd87f2d43ad783c0aa17
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-08-16 07:26:48 +0700

    Release v0.01

--- lib/Dist/Zilla/Plugin/Rinci/ScriptFromFunc.pm
+++ lib/Dist/Zilla/Plugin/Rinci/ScriptFromFunc.pm
@@ -149,7 +149,7 @@ sub gather_files {
             ($self->snippet_before_instantiate_cmdline ? "# snippet_before_instantiate_cmdline\n" . $self->snippet_before_instantiate_cmdline . "\n\n" : ""),
             "$cmdline_mod->new(\n",
             "    url => ", dump($url), ",\n",
-            (defined($scriptspec{log_any_app}) ? "    log_any_app => ", dump($scriptspec{log_any_app}), ",\n",
+            (defined($scriptspec{log_any_app}) ? "    log_any_app => " . dump($scriptspec{log_any_app}) . ",\n" : ""),
             ")->run;\n",
             "\n",
         );
